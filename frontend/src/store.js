328b2d083efb8ff13281e4221f129ff9c5743ecf
Meistro <meistro57@gmail.com>
2025-07-08 15:20:33 -0500

    Add search to NodePalette and extend workflow store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,8 +3,14 @@ import { create } from 'zustand'
 export const useWorkflowStore = create(set => ({
   nodes: [],
   edges: [],
+  selectedNode: null,
   setNodes: nodes => set({ nodes }),
   setEdges: edges => set({ edges }),
   addNode: node => set(state => ({ nodes: [...state.nodes, node] })),
-  addEdge: edge => set(state => ({ edges: [...state.edges, edge] }))
+  addEdge: edge => set(state => ({ edges: [...state.edges, edge] })),
+  selectNode: nodeId => set({ selectedNode: nodeId }),
+  updateNode: (id, updates) =>
+    set(state => ({
+      nodes: state.nodes.map(n => (n.id === id ? { ...n, ...updates } : n))
+    }))
 }))
